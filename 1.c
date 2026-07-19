64830003d44e4740dbdd76b8af0924ed31d58af8
leeah06 <leeah@sookmyung.ac.kr>
2026-07-19 22:33:58 +0900

    4

--- 1.c
+++ 1.c
@@ -2,17 +2,19 @@
 
 int main(void)
 {
-    char c;
-    char next_c;
+    float conversion_a, conversion_b; // 유의사항에 맞추어 부동소수점 변수로 선언
+    float result;
 
-    printf("enter a character: ");
-    scanf("%c", &c);
+    printf("분자를 입력하세요 : ");
+    scanf("%f", &conversion_a);
 
-    // 다음 문자를 계산
-    next_c = c + 1;
+    printf("분모를 입력하세요 : ");
+    scanf("%f", &conversion_b);
 
-    // c와 next_c를 각각 문(%c)와 정수(%i) 형태로 출력
-    printf("the next character of %c (%i) is %c (%i)\n", c, c, next_c, next_c);
+    // 나누기 연산 수행
+    result = conversion_a / conversion_b;
+
+    printf("나누기의 결과는 %f 입니다.\n", result);
 
     return 0;
 }
